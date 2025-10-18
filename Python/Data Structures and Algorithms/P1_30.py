64d4f9ee53f445ee01f788549ed67d63e23f502f
R-Ed153 <edmundmunene@gmail.com>
2025-10-18 09:31:21 +0300

    Did project 1.30 of Data Structures and Algorithms in Python.

---
+++ Python/Data Structures and Algorithms/P1_30.py
@@ -0,0 +1,14 @@
+def numberOfTimesDivisibleBy2(n:int)->int:
+    counter = 0
+    while(n >= 2):
+        n = n // 2
+        counter += 1
+    return counter
+
+
+
+
+if(__name__ == "__main__"):
+    while True:
+        userInput = int(input("Enter a positive number to find out how mainy times 2 can be divided from it: \n"))
+        print(numberOfTimesDivisibleBy2(userInput))
