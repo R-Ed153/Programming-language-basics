6103f8a694289634f51af58c4b786ef2da168142
R-Ed153 <edmundmunene@gmail.com>
2025-11-11 22:28:58 +0300

    Read on sorted and unsorted priority queues

---
+++ Python/Data Structures and Algorithms/Chapter9/unsortedPriorityQueue.py
@@ -0,0 +1,35 @@
+from priorityQueueBase import PriorityQueueBase
+from utility import Empty
+
+class UnsortedPriorityQueue(PriorityQueueBase):
+    def __init__(self):
+        self._data = PositionalList()
+
+    def _find_min(self):
+        if self.is_empty:
+            raise Empty('Priority queue is empty')
+        small = self._data.first()
+        walk = self._data.after(small)
+
+        while walk is not None:
+            if walk.element()<small.element():
+                small = walk
+            walk = self._data.after(walk)
+        return small    
+
+    def __len__(self):
+        return len(self._data)
+    
+    def add(self,key,value):
+        self._data.add_last(self._Item(key,value))
+
+    def min(self):
+        p = self._find_min()
+        item = p.element()
+        return (item._key,item._value)
+    
+    def remove_min(self):
+        p = self._find_min()
+        item = self._data.delete(p)
+        return(item._key,item._value)
+
