f6497abfbe9226c2324df08c1ef661e76e8fd1c7
R-Ed153 <edmundmunene@gmail.com>
2025-11-18 22:25:34 +0300

    Started work on maps and read on hash map theory.

---
+++ Python/Data Structures and Algorithms/Chapter10/mapBase.py
@@ -0,0 +1,20 @@
+from collections.abc import MutableMapping
+
+class MapBase(MutableMapping):
+    class _Item:
+        __slots__ = "_key","_value"
+
+        def __init__(self,k,v):
+            self._k = k
+            self._v = v
+
+        def __eq__(self,other):
+            return self._key == other._key
+        
+        def __ne__(self,other):
+            return not (self == other)
+        
+        def __lt__(self,other):
+            return self._key < other._key
+            
+print(list("Ed"))
