a50b8b71e0dca3c508b387a318b9d64231bd6466
R-Ed153 <edmundmunene@gmail.com>
2025-11-25 21:59:39 +0300

    Did work on multiMaps and sortedMaps

---
+++ Python/Data Structures and Algorithms/Chapter10/costPerformanceDatabase.py
@@ -0,0 +1,18 @@
+from sortedTableMap import SortedTableMap
+
+class CostPerformanceDatabase:
+    def __init__(self):
+        self._M = SortedTableMap()
+
+    def best(self,c):
+        return self._M.find_le(c)
+    
+    def add(self,c,p):
+        other = self._M.find_le(c)
+        if other is not None and other[1] >= p:
+            return
+        self._M[c] = p
+        other = self._M.find_gt(c)
+        while other is not None and other[1] <= p:
+            del self._M[other[0]]
+            other = self._M.find_gt(c)
--- Python/Data Structures and Algorithms/Chapter10/sortedTableMap.py
+++ Python/Data Structures and Algorithms/Chapter10/sortedTableMap.py
@@ -2,59 +2,89 @@ from mapBase import MapBase
 
 
 class SortedTableMap(MapBase):
-    def _find_index(self,k,low,high):
+    def _find_index(self, k, low, high):
         if high < low:
             return high + 1
         else:
-            mid = (low + high)//2
+            mid = (low + high) // 2
             if k == self._table[mid]._key:
                 return mid
             elif k < self._table[mid]._key:
-                return self._find_index(k,low,mid-1)
+                return self._find_index(k, low, mid - 1)
             else:
-                return self._find_index(k,mid+1,high)
-            
+                return self._find_index(k, mid + 1, high)
+
     def __init__(self):
         self._table = []
 
     def __len__(self):
         return len(self._table)
-    
+
     def __getitem__(self, key):
-        j = self._find_index(key,0,len(self._table)-1)
+        j = self._find_index(key, 0, len(self._table) - 1)
         if j == len(self._table) or self._table[j]._key != key:
-            raise KeyError('Key Error:'+ repr(key))
+            raise KeyError("Key Error:" + repr(key))
         return self._table[j]._value
-    
+
     def __setitem__(self, key, value):
-        j = self._find_index(key,0,len(self._table)-1)
+        j = self._find_index(key, 0, len(self._table) - 1)
         if j < len(self._table) and self._table[j]._key == key:
             self._table[j]._value = value
 
     def __delitem__(self, key):
-        j = self._find_index(key,0,len(self._table)-1)
-        if j == len(self._table) or self._table[j]._key!=key:
-            raise KeyError('Key Error' + repr(key))
-        self._table.pop(j)  
+        j = self._find_index(key, 0, len(self._table) - 1)
+        if j == len(self._table) or self._table[j]._key != key:
+            raise KeyError("Key Error" + repr(key))
+        self._table.pop(j)
 
     def __iter__(self):
         for item in self._table:
             yield item._key
-    
+
     def __reversed__(self):
         for item in reversed(self._table):
             yield item._key
-        
+
     def find_min(self):
         if len(self._table) > 0:
-            return (self._table[0]._key,self._table[0]._value)
+            return (self._table[0]._key, self._table[0]._value)
         else:
             return None
-    
+
     def find_max(self):
         if len(self._table) > 0:
-            return (self._table[-1]._key,self._table[-1]._value)
+            return (self._table[-1]._key, self._table[-1]._value)
         else:
             return None
-        
-    
+
+    def find_ge(self, k):
+        j = self._find_index(k, 0, len(self._table) - 1)
+        if j < len(self._table):
+            return (self._table[j]._key, self._table[j]._value)
+        else:
+            return None
+
+    def find_lt(self, k):
+        j = self._find_index(k, 0, len(self._table) - 1)
+        if j > 0:
+            return (self._table[j - 1]._key, self._table[j - 1]._value)
+        else:
+            return None
+
+    def find_gt(self, k):
+        j = self._find_index(k, 0, len(self._table) - 1)
+        if j < len(self._table) and self._table[j]._key == k:
+            j += 1
+        if j < len(self._table):
+            return (self._table[j]._key, self._table[j]._value)
+        else:
+            return None
+
+    def find_range(self, start, stop):
+        if start is None:
+            j = 0
+        else:
+            j = self._find_index(start, 0, len(self._table) - 1)
+        while j < len(self._table) and (stop is None or self._table[j]._key < stop):
+            yield (self._table[j]._key, self._table[j]._value)
+            j += 1
