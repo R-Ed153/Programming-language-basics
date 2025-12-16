9a2a330ca7fd45ae8e34c0fe35e166af0d221eb4
R-Ed153 <edmundmunene@gmail.com>
2025-12-16 21:57:33 +0300

    Read on string patter matching.

---
+++ Python/Data Structures and Algorithms/Chapter13/findBrute.py
@@ -0,0 +1,9 @@
+def find_brute(T, P):
+    n, m = len(T), len(P)
+    for i in range(n - m + 1):
+        k = 0
+        while k < m and T[i + k] == P[k]:
+            k += 1
+        if k == m:
+            return i
+    return -1
